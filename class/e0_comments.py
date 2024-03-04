5b5d8fc8bb1c0bbc2230d841f01083e63b7b0e7d
yahircasas <ycasasvaldez@gmail.com>
2024-03-04 12:11:17 -0600

    exO and ex1 done

--- class/e0_comments.py
+++ class/e0_comments.py
@@ -1,10 +1,24 @@
-a = 5
-
+a = 5 # (inline comment) this is an instance of an object
+"""
+This is a block comment
+We can add a lot of lines
+"""
 
 def f(x):
-    return x * x
+    """
+    this is a docstring
+    A docstring explains what a function does
+    :param x:
+    :return:
+    """
+    return x * x  # Returns the square of x
 
+# This comment is outside the if statement
 
 for item in 'python':
+    # An indented comment
     if item == 'p':
-        print('Found!')
+        # An indented comment
+        print('Found!')  # A line comment
+
+# This comment is outside the if statement
--- class/e1_helloworld.py
+++ class/e1_helloworld.py
@@ -1,20 +1,26 @@
 # Instructions: print out the following sentences, the sentence must show as the comments
 
 # "Once Charles Dickens said: "It was the best of times, it was the worst of times""
-
-
+print('"Once Charles Dickens said: "It was the best of times, it was the worst of times""')
+print("\"Once Charles Dickens said: \" It was the best of times, it was the worst of times\"")
+print()
 
 # 5    7    9    11
-
+print(5, 7, 9, 11, sep = '\t')
+print()
 
 """
 Actuarial
 Science
 """
-
+print('Actuarial', sep='\n')
+print('Science')
+print()
+print('Actuarial', 'Science', sep='\n')
 
 # With just three '-' print out five '-'
-
+print('-','-','-', sep='-')
+print()
 
 # Hello world (this is the first program that all programmers do)
-
+print('Hello world')
