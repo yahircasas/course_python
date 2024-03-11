73a936dda9765143c5e065bfec3468833491377c
yahircasas <ycasasvaldez@gmail.com>
2024-03-10 22:32:27 -0600

    exercise 3 done

---
+++ class/e3_creating_variables.py
@@ -0,0 +1,47 @@
+# creación de variables
+
+# creación de un String
+nombre_licenciatura = "Actuaría"
+
+# creación de un Int
+edad_decimal = 21
+
+# creación de un Float
+numero_pi = 3.1415926535
+
+# creación de una lista
+numeros_favoritos = [22,10,2002]
+
+# creación de una tuple
+nombres_hermanos = ("Nancy", "José Luis")
+
+# creación de un set
+numeros_multiplos = {5,10,15,20,25}
+
+# creación de una dictionary
+informacion_estudiante = {"Nombre": "Yahir Eduardo", "Apellido": "Casas Valdez", "Edad": 21, "Licenciatura": "Actuaría",
+                          "No. Cuenta": 1710149}
+
+# creación de un complex
+numero_complejo = 1 + 2j
+
+# Imprime una lista de números favoritos
+print("Lista:", numeros_favoritos)
+
+# Imprime un entero que representa una edad en formato decimal
+print("Entero:", edad_decimal)
+
+# Imprime una tupla de nombres de hermanos
+print("Tupla:", nombres_hermanos)
+
+# Imprime un número decimal que representa el valor de pi
+print("Decimal:", numero_pi)
+
+# Imprime una cadena que representa el nombre de una licenciatura
+print("Cadena:", nombre_licenciatura)
+
+# Imprime un diccionario que contiene información sobre un estudiante
+print("Diccionario:", informacion_estudiante)
+
+# Imprime un conjunto de números que son múltiplos de algún número
+print("Conjunto:", numeros_multiplos)
